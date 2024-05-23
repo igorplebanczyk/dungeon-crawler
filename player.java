95fb2c8350b9190f79c7f947ccb048d0fd8c0d7f
Igor Plebańczyk <114337770+igorplebanczyk@users.noreply.github.com>
2024-05-23 13:04:13 +0200

    Create player.java

---
+++ player.java
@@ -0,0 +1,26 @@
+public class Player {
+    private int x;
+    private int y;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public int[] getPosition() {
+        return new int[]{x, y};
+    }
+}
