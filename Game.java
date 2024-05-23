3fa2f00a23bd72a5e8cf4741b9318e7e233023c5
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-05-23 16:40:35 +0200

    Add textures, messages and .gitignore

--- Game.java
+++ Game.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.Objects;
+import javax.imageio.ImageIO;
 
 public class Game extends JFrame {
     private static final int TILE_SIZE = 64;
@@ -12,8 +15,18 @@ public class Game extends JFrame {
 
     public int level = 1;
 
+    private Image playerImage;
+    private Image wallImage;
+    private Image floorImage;
+    private Image exitImage;
+
+    private String message;
+    private Timer messageTimer;
+
     public Game() {
+        loadImages();
         initializeGame();
+        showLevelAnnouncement();
         setTitle("Dungeon Crawler - Level " + level);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
@@ -46,6 +59,7 @@ public class Game extends JFrame {
                     initializeGame();
                     level++;
                     setTitle("Dungeon Crawler - Level " + level);
+                    showLevelAnnouncement();
                 }
 
                 repaint();
@@ -56,6 +70,17 @@ public class Game extends JFrame {
         adjustWindowSize();
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private void loadImages() {
+        try {
+            playerImage = ImageIO.read(Objects.requireNonNull(getClass().getResource("/images/player.png")));
+            wallImage = ImageIO.read(Objects.requireNonNull(getClass().getResource("/images/wall.png")));
+            floorImage = ImageIO.read(Objects.requireNonNull(getClass().getResource("/images/floor.png")));
+            exitImage = ImageIO.read(Objects.requireNonNull(getClass().getResource("/images/exit.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void initializeGame() {
@@ -72,27 +97,55 @@ public class Game extends JFrame {
         setSize(frameWidth, frameHeight);
     }
 
+    private void showLevelAnnouncement() {
+        message = "Welcome to Level " + level;
+        if (messageTimer != null) {
+            messageTimer.stop();
+        }
+        messageTimer = new Timer(2000, e -> {
+            message = null;
+            repaint();
+        });
+        messageTimer.setRepeats(false);
+        messageTimer.start();
+        repaint();
+    }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
                 char tile = dungeon.getTile(x, y);
-
-                if (tile == '#') {
-                    g.setColor(Color.BLACK);
-                } else if (tile == '.') {
-                    g.setColor(Color.WHITE);
-                } else if (tile == 'P') {
-                    g.setColor(Color.BLUE);
-                } else if (tile == 'E') {
-                    g.setColor(Color.RED);
+                Image imageToDraw = null;
+                switch (tile) {
+                    case '#':
+                        imageToDraw = wallImage;
+                        break;
+                    case '.':
+                        imageToDraw = floorImage;
+                        break;
+                    case 'P':
+                        imageToDraw = playerImage;
+                        break;
+                    case 'E':
+                        imageToDraw = exitImage;
+                        break;
                 }
-                g.fillRect(x * TILE_SIZE + getInsets().left, y * TILE_SIZE + getInsets().top, TILE_SIZE, TILE_SIZE);
-                g.setColor(Color.GRAY);
-                //g.drawRect(x * TILE_SIZE + getInsets().left, y * TILE_SIZE + getInsets().top, TILE_SIZE, TILE_SIZE);
+                if (imageToDraw != null) {
+                    g.drawImage(imageToDraw, x * TILE_SIZE + getInsets().left, y * TILE_SIZE + getInsets().top, TILE_SIZE, TILE_SIZE, this);
+                }
             }
         }
+        if (message != null) {
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Times ", Font.BOLD, 56));
+            FontMetrics fm = g.getFontMetrics();
+            int messageWidth = fm.stringWidth(message);
+            int messageHeight = fm.getHeight();
+            int x = (getWidth() - messageWidth) / 2;
+            int y = (getHeight() - messageHeight) / 2 + fm.getAscent();
+            g.drawString(message, x, y);
+        }
     }
-
 }
